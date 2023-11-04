cf12812545edd1005d3fd78f4562c6e8737efa71
Emad Ali Khan <emadalikhan5@gmail.com>
2023-11-04 15:40:44 +0500

    work Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import Button  from "./components/button";
+import {useEffect,useState} from 'react'
+import axios from 'axios'
 function App() {
+  const[productData,setProductData] = useState([])
+  const[isLoading,setIsLoading]= useState(false)
+  const getData = async()=>{
+    setIsLoading(true)
+    try{
+      const response =  await axios.get("https://fakestoreapi.com/products/")
+      setIsLoading(false)
+      console.log(response.data);
+      setProductData(response.data)
+    }catch{
+      setIsLoading(false)
+      console.log("err");
+    }
+  }
+  useEffect(()=>{
+getData()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="mainContainer">
+  {
+    isLoading?<img src='https://i.stack.imgur.com/8puiO.gif'/>:productData.map((e,i)=><div key={i} className='cardParent'>
+  <section className='card'>
+  <img src={e.image} width={200}/>
+  <h3 style={{fontSize:22}}>{e.title}</h3>
+  <hr/>
+  <p style={{fontWeight:"bolder"}}>{e.category}</p>
+  <p>{e.description}</p>
+      
+  <Button label={e.price}/>
+  </section>
+  </div>
+)
+  }
+</div>
   );
 }
-
 export default App;
---
+++ src/components/button.jsx
@@ -0,0 +1,6 @@
+import "./button.css"
+const Button = ({label})=>{
+    return<button>Buy {label}$</button>
+    
+}
+export default Button
